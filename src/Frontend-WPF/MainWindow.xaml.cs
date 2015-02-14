7b4e98a189dc0cbd0a617a3b8f8f652c3e279c2e
Kim-Maximilian Nürnberger <git@kim-nuernberger.eu>
2015-02-14 18:55:54 +0100

    Removed old WPF, created new clean Project

--- src/Frontend-WPF/MainWindow.xaml.cs
+++ src/Frontend-WPF/MainWindow.xaml.cs
@@ -1,34 +1,6 @@
-﻿/*
- * $Id$
- * $URL$
- * $Rev$
- * $Author$
- * $Date$
- *
- * smuxi - Smart MUltipleXed Irc
- *
- * Copyright (c) 2007 Mirco Bauer <meebey@meebey.net>
- * Copyright (c) 2007 Jeffrey Richardson <themann@indyfantasysports.net>
- *
- * Full GPL License: <http://www.gnu.org/licenses/gpl.txt>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- */
-
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -37,30 +9,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Smuxi.Frontend.Wpf
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaktionslogik für MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        public WpfUI UI { get; set; }
-
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        public TextBlock NetworkStatusbar { get {return _NetworkStatus; }}
-        public TextBlock Statusbar { get { return _Status; } }
-
-        public void ApplyConfig(Smuxi.Engine.UserConfig UserConfig)
-        {
-            
-        }
-
-        public TextBox Entry { get { return null; } }
     }
 }
